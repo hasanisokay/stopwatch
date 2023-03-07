3eaa82b048519d0280e75c2ed536bc210ff837a6
hasanisokay <hasanisokay@gmail.com>
2023-03-07 09:06:24 +0600

    added milisecond

--- js.js
+++ js.js
@@ -1,18 +1,47 @@
-const stopwatch = document.getElementById("stopwatch")
-let num = 0;
-let intervalID; 
+const min = document.getElementById("stopwatch-min")
+const second = document.getElementById("stopwatch-second")
+const hours = document.getElementById("stopwatch-hours")
+const ms = document.getElementById("stopwatch-milisecond")
+let secondCounter = 0;
+let msCount = 0;
+let intervalID;
+let intervalID2
+let started = false
 function startStopwatch(){
-    intervalID = setInterval(displayStopwatch, 1000);
+    if (started ==false){
+        started = true;
+        intervalID2 = setInterval( displayMilisecond ,16.67);
+        intervalID = setInterval(displayStopwatch, 1000);
+    }
+    else{
+        return
+    }
+}
+function displayMilisecond(){
+    if(msCount ==100){
+        msCount = 0;
+    }
+    ms.innerText = msCount++
 }
 function displayStopwatch(){
-    stopwatch.innerText = num;
-    num ++
+    if (secondCounter ===60){
+        let minPrevious = parseInt(min.innerText)
+        secondCounter =0
+        min.innerText = minPrevious + 1
+    }
+    second.innerText = secondCounter++
+
 }
 function stopStopwatch(){
     clearTimeout(intervalID)
+    clearTimeout(intervalID2)
+    started = false
 }
 function resetStopwatch(){
     stopStopwatch()
-    stopwatch.innerText = 0;
-    num = 0; 
+    second.innerText = "00" 
+    min.innerText ="00" 
+    hours.innerText ="00" 
+    secondCounter = 0
+    msCount = 0
 }
